2220bd550e079d1c25958ca2ed46e59f051de42c
neliswillers@gmail.com <neliswillers@gmail.com@b3a66297-ed53-8ea1-4331-0047dcb9a0cc>
2014-03-21 10:51:51 +0000

    Updated spectral density conversions to now also work with (N,1) vectors.

--- trunk/pyradi/__init__.py
+++ trunk/pyradi/__init__.py
@@ -32,7 +32,8 @@ http://pyradi.googlecode.com/svn//trunk/doc/_build/html/index.html
 Visit the google group at http://groups.google.com/group/pyradi-dev
 
 """
-__version__= "0.1.36" # svn $Revision$ 
+__version__= "0.1.36"
+__versionSVN__= "0.1.36 "+"svn $Revision$"
 __author__='pyradi team'
 
 #import ry3dnoise
