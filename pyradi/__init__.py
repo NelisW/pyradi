3d6907ba6344f7586be5883e8b6e5847c17014a2
neliswillers@gmail.com <neliswillers@gmail.com@b3a66297-ed53-8ea1-4331-0047dcb9a0cc>
2014-11-28 09:21:26 +0000

    Updated documentation

--- pyradi/__init__.py
+++ pyradi/__init__.py
@@ -38,7 +38,7 @@ computational radiometry (flux flow) calculations.
     | https://github.com/NelisW/ComputationalRadiometry#computational-optical-radiometry-with-pyradi
 
 """
-__version__= "0.1.53"
+__version__= "0.1.54"
 __versionSVN__= "svn $Revision$"
 __author__='pyradi team'
 
